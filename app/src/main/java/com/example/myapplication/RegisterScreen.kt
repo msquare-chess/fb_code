271e8d32176a88134a66db635c9a3b0ea665c74f
Magic_Squares_ <themagicsquarez@gmail.com>
2025-09-14 17:46:49 +0530

    3 register screen modified

--- app/src/main/java/com/example/myapplication/RegisterScreen.kt
+++ app/src/main/java/com/example/myapplication/RegisterScreen.kt
@@ -6,7 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class Register_Screen : AppCompatActivity() {
+class RegisterScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
