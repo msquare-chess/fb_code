621acbbaf723fccacd910b5a7ff05bcad5beced4
Magic_Squares_ <themagicsquarez@gmail.com>
2025-09-14 17:45:33 +0530

    3 Login Screen modified

--- app/src/main/java/com/example/myapplication/Register_Screen.kt
+++ app/src/main/java/com/example/myapplication/Register_Screen.kt
@@ -11,6 +11,7 @@ class Register_Screen : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_register_screen)
+        //setContentView(R.layout.activity_register_screen)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
